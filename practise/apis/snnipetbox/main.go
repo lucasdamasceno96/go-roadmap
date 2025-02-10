feb8a26eb877caae65f279b4111227c62d8f49ff
Lucas Damasceno <lucasdamascenopinto96@gmail.com>
2025-02-10 20:30:08 -0300

    eroor text page

--- practise/apis/snnipetbox/main.go
+++ practise/apis/snnipetbox/main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from snnipetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display an specic snnipet from snippetbox"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snnippet from snipetbox"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
